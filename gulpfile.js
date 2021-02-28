6fd020735753ac43992f9fe9645eb0e8c7b7ac7c
SignpostMarv <me@signpostmarv.name>
2021-02-28 10:46:09 +0000

    switching to postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 const gulp = require('gulp');
 const typescript = require('gulp-typescript');
+const postcss = require('gulp-postcss');
+const postcss_plugins = {
+    nested: require('postcss-nested'),
+};
+const rename = require('gulp-rename');
 
 gulp.task('typescript', () => {
     return gulp.src('./src/**/*.ts').pipe(
@@ -8,3 +13,22 @@ gulp.task('typescript', () => {
         )()
     ).pipe(gulp.dest('./src/'));
 });
+
+gulp.task('postcss', () => {
+    return gulp.src('./src/**/*.postcss').pipe(
+        postcss([
+            postcss_plugins.nested(),
+        ])
+    ).pipe(
+		rename({
+			extname: '.css'
+		})
+    ).pipe(gulp.dest('./src/'));
+});
+
+gulp.task('default', gulp.series(...[
+    gulp.parallel(...[
+        'typescript',
+        'postcss',
+    ]),
+]));
